4bdd9ad67c967147297eebab84eef92fdd7951ad
OlguinJv <reminiscent.nightlight@gmail.com>
2018-05-29 05:26:16 -0300

    logica de registro

--- registrate.php
+++ registrate.php
@@ -1,4 +1,56 @@
-<?php
+<?php session_start();
+
+if (isset($_SESSION['usuario'])) {
+  header('Location: index.php');
+};
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' ) {
+  $usuario = filter_var(strtolower($_POST['usuario']), FILTER_SANITIZE_STRING);
+  $password = $_POST['password'];
+  $password2 = $_POST['password2'];
+
+
+
+  $errores = '';
+
+  // comprueba si alguno de los 3 campos es incorrecto
+  if (empty($usuario) or empty($password) or empty($password2)) {
+    $errores .= '<li>Por favor llena todos los datos correctamente.</li>';
+  } else {
+    try {
+      $conexion = new PDO('mysql:host=localhost;dbname=curso_login', 'root', '');
+    } catch (PDOException $e) {
+      echo "Error: " . $e->getMessage();
+    }
+
+    $statement = $conexion->prepare('SELECT * FROM usuarios WHERE usuario = :usuario LIMIT 1');
+    $statement->execute(array(':usuario' => $usuario));
+    $resultado = $statement->fetch();
+
+    if ($resultado != false) {
+      $errores .= '<li>El nombre de usuario ya existe</li>';
+    }
+
+    $password = hash('sha512', $password);
+    $password2 = hash('sha512', $password2);
+    // echo $usuario . $password . '<br />' . $password2;
+
+    if ($password != $password2) {
+      $errores .= '<li>Las contraseñas no son iguales</li>';
+    }
+  }
+
+  if ($errores == ''){
+    $statement = $conexion->prepare('INSERT INTO usuarios (id, usuario, pass) VALUES (null, :usuario, :pass)');
+    $statement->execute(array(
+      ':usuario' => $usuario,
+      ':pass'=> $password
+    ));
+
+    header('Location: login.php');
+  }
+
+};
 
 require 'vistas/registrate.view.php';
 
--- vistas/registrate.view.php
+++ vistas/registrate.view.php
@@ -28,6 +28,14 @@
           <i class="submit-btn fa fa-arrow-right" onclick="login.submit()"></i>
         </div>
 
+        <?php if (!empty($errores)): ?>
+          <div class="error">
+            <ul>
+              <?php echo $errores; ?>
+            </ul>
+          </div>
+        <?php endif; ?>
+
       </form>
 
       <p class="texto-registrate">¿ Ya tienes cuenta ?
